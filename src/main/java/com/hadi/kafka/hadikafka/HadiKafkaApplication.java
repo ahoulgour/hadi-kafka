9c141744b2123e2e1be0d11195418b36decda42c
Gor <ahoulgour@gmail.com>
2020-02-26 21:40:55 +0100

    structure

---
+++ src/main/java/com/hadi/kafka/hadikafka/HadiKafkaApplication.java
@@ -0,0 +1,13 @@
+package com.hadi.kafka.hadikafka;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HadiKafkaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HadiKafkaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/hadi/kafka/hadikafka/HadiKafkaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hadi.kafka.hadikafka;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HadiKafkaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
